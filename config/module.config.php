acbc0e4e9d129d25df947af3c8f5a71e3c256440
Errol A. Zuñiga Jr <errol.zuniga@coredna.com>
2015-06-02 16:46:41 +0800

    Enhances acl

--- config/module.config.php
+++ config/module.config.php
@@ -28,6 +28,31 @@ return array(
 			'BjyAuthorize\Guard\Controller' => array(
 				array('controller' => 'Form', 'roles' => array('user', 'guest')),
                 array('controller' => 'Admin\Form', 'roles' => array('user', 'admin')),
+                array('controller' => 'Admin\Form', 'action'=>'manage', 'roles' => array('admin')),
+
+                array('controller' => 'Admin\Form', 'action'=>'create-form', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'edit-form', 'roles' => array('admin')),
+
+                array('controller' => 'Admin\Form', 'action'=>'create-section', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'edit-section', 'roles' => array('admin')),
+
+                array('controller' => 'Admin\Form', 'action'=>'create-fieldset', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'create-fieldset-as-child', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'edit-fieldset-as-child', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'edit-fieldset', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'delete-fieldset', 'roles' => array('admin')),
+
+                array('controller' => 'Admin\Form', 'action'=>'results', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'form-results', 'roles' => array('admin','owner')),
+
+                array('controller' => 'Admin\Form', 'action'=>'create-element', 'roles' => array('admin')),
+                array('controller' => 'Admin\Form', 'action'=>'delete-element', 'roles' => array('admin')),
+
+                array('controller' => 'Admin\Form', 'action'=>'submitted-forms', 'roles' => array('admin','owner')),
+                array('controller' => 'Admin\Form', 'action'=>'user-form-data', 'roles' => array('admin','owner')),
+
+                array('controller' => 'Admin\Form', 'action'=>'reports', 'roles' => array('admin','owner')),
+                array('controller' => 'Admin\Form', 'action'=>'download-csv', 'roles' => array('admin','owner')),
             ),
 		),
 	),
